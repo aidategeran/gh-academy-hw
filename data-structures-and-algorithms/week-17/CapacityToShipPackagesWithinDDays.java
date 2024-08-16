ca55e259817454256153ab208cc711e98a34c37e
aidategeran <164016510+aidategeran@users.noreply.github.com>
2024-08-16 16:15:02 -0500

    Create CapacityToShipPackagesWithinDDays.java

---
+++ data-structures-and-algorithms/week-17/CapacityToShipPackagesWithinDDays.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int D) {
+        int left = 0;
+        int right = 0;
+        int answer = -1;
+
+        for (int w : weights ) {
+            left = Math.max(left,w);
+            right += w;
+        }
+        while (left <= right) {
+            int middle = (left+right)/2;
+            int need=1;
+            int cur = 0;
+            for (int w : weights ){
+                if (cur+w > middle){
+                    need+=1;
+                    cur = w;
+                }else {
+                    cur +=w;
+                }
+            
+            if (need > D) {
+                left = middle+1;
+            }else{
+                answer = middle;
+                right = middle-1;
+            }
+        
+         
+
+
+            }
+            
+            
+
+        }return answer;
+    }
+}
