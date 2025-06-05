e18167886be27a23967f6c9b9f65bef51872b16b
aidategeran <164016510+aidategeran@users.noreply.github.com>
2025-06-04 21:25:39 -0500

    Create 1833MaximumIceCreamBars.java

---
+++ data-structures-and-algorithms/week-25/1833MaximumIceCreamBars.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        //             v
+        // 1, 1, 2, 3, 4
+        // coins = 0
+        // answer = 4
+        int n = costs.length;
+        int[] count = new int[100000 + 1]; // 0..100_000
+        for (int x : costs) {
+            count[x]++;
+        }
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                costs[index] = i;
+                index++;
+                count[i]--;
+            }
+        }
+        int ice_cream = 0;
+        while (ice_cream < n && costs[ice_cream] <= coins) {
+            coins -= costs[ice_cream];
+            ice_cream++;
+        }
+        return ice_cream;
+    }
+}
